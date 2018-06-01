af2165f69b430f20a59e569d62f63742a194947d
bfire2015 <bfire2013@gmail.com>
2018-06-01 09:02:37 +0800

    m by bfire for 2018-06-01 0902 更新资源

---
+++ scikit_learn/sptest/estest.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'BfireLai'
+__mtime__ = '2018/4/19'
+"""
+from elasticsearch import Elasticsearch
+import time
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import json
+
+#设置字符集,防止中文乱码
+mpl.rcParams['font.family'] = 'sans-serif'
+mpl.rcParams['font.sans-serif'] = 'SimHei'
+mpl.rcParams['axes.unicode_minus'] = False
+
+#test
+es = Elasticsearch([{'host':'192.168.56.104', 'port':9200}])
+#online
+#es = Elasticsearch([{'host':'120.79.82.52', 'port':9200}])
+indexs = time.strftime("%Y-%m")
+#indexs = '2018-04'
+if es.indices.exists(index=indexs) is not True:
+	es.indices.create(index=indexs)
+
+print('create doc befor')
+#es.delete(index='indexName', doc_type='typeName', id='idValue')
+#es.delete(index=indexs, doc_type='test', id='1')
+
+doc = {'t':'da','log':'b','name':'bfire','txt':'bfire test 1', 'ts':int(time.time()), 'dt': time.strftime("%Y-%m-%d %H:%M:%S")}
+#es.index(index=indexs, doc_type='test', refresh=True, body=doc)
+print('create doc after')
+_where = {
+	'query':{
+		'match_all':{}
+	}
+}
+#info = es.search(index=indexs, doc_type='test', body=_where)
+info = {'hits':{'hits':{'_source':['d']}}}
+print('search mathc_all')
+print(info)
+print('hit info:')
+for hit in info['hits']['hits']:
+	#print(hit['_source'])
+	pass
+
+path = "./boss.json"  # 数据文件路径
+file = open(path, 'r', encoding='utf-8')
+json_info = json.load(file)
+#print(json_info)
+for i in json_info:
+	print(i)
+print('all the end')
+
+
+
---
+++ scikit_learn/sptest/sptest/spiders/lp_spider.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'BfireLai'
+__mtime__ = '2018/05/30'
+"""
+import scrapy
+
+from ..items import SptestItem
+
+class LpSpider(scrapy.Spider):
+	name = 'lp'
+	allowed_domains = ['boss.com']
+	headers = {
+		'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+		'Host':'www.zhipin.com',
+		'Referer':'https://www.zhipin.com/job_detail/?query=ai&scity=101280600&industry=&position=',
+	}
+
+	def start_requests(self):
+		urls = [
+			#'https://www.zhipin.com/job_detail/?query=ai&scity=101280600&industry=&position=',
+			'https://www.zhipin.com/c101280600/h_101280600/?query=ai&ka=recommend-job-list'
+		]
+		for url in urls:
+			yield scrapy.Request(url=url, headers=self.headers, callback=self.parse)
+
+	def parse(self, response):
+		#print(response)
+#下载
+#		filename = response.url.split("/")[-1]
+#		with open(filename, 'wb') as f:
+#			f.write(response.body)
+#获取数据
+		item = SptestItem()
+		a_space = response.xpath('//div[@class="job-primary"]')
+		#print(a_space)
+		for a in a_space:
+			item['title'] = a.xpath('div[@class="info-primary"]/*/a/div[@class="job-title"]/text()').extract()
+			item['money'] = a.xpath('div[@class="info-primary"]/*/a/span[@class="red"]/text()').extract()
+			item['descs'] = a.xpath('div[@class="info-primary"]/*/a/span/text()').extract()
+			item['wtime'] = a.xpath('div[@class="info-primary"]/p/text()').extract()
+			item['company'] = a.xpath('div[@class="info-company"]/*/h3/a/text()').extract()
+			item['ptime'] = a.xpath('div[@class="info-publis"]/p/text()').extract()
+			print(item['descs'])
+			yield item
